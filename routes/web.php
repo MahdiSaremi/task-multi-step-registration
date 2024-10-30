1f5448bbf0357fc375b18d628660734a9fa8c4e0
Mahdi Saremi <mahdisarmi00@gmail.com>
2024-10-30 22:06:57 +0330

    install api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\UserFormController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/user/{user}', );
+
+Route::controller(UserFormController::class)->group(function () {
+
+
+    Route::get('/state/{state}', 'state');
+
 });
