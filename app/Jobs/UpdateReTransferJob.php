7b2d19c8a07c1ec5c9c142789b1492560745e248
Mahdi Saremi <mahdisarmi00@gmail.com>
2024-10-31 19:24:56 +0330

    added re-transfer job

---
+++ app/Jobs/UpdateReTransferJob.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Secondary\Contracts\ApiClient;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class UpdateReTransferJob implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(
+        public mixed $id,
+        public array $data,
+    )
+    {
+    }
+
+    /**
+     * Retry update request
+     */
+    public function handle(): void
+    {
+        try
+        {
+            app(ApiClient::class)->update($this->id, $this->data);
+        }
+        catch (\Throwable)
+        {
+            $this->release(now()->addMinutes(2 ** $this->attempts() / 4));
+        }
+    }
+}
